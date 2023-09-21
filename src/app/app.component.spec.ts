c27002efbe070541de24a8e7244bda7fea5e2eea
Gaelle327 <gaellesaid65@gmail.com>
2023-09-21 21:46:21 +0300

    update workflowt

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { CalculatorComponent } from './calculator/calculator.component';
+import { FormsModule } from '@angular/forms';
 
 describe('AppComponent', () => {
   beforeEach(() => TestBed.configureTestingModule({
-    imports: [RouterTestingModule],
-    declarations: [AppComponent]
+    imports: [RouterTestingModule, FormsModule],
+    declarations: [AppComponent, CalculatorComponent]
   }));
 
   it('should create the app', () => {
@@ -24,6 +26,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-project app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Angular calculator app is running!');
   });
 });
